0ac76c718f8268e1c8b6edd808db254c4acc6a88
Qianqian Zhao <chance.zhao@gmail.com>
2022-12-06 15:18:45 -0500

    day 4 part 2 any overlapping cleaning duty

--- day4/index.js
+++ day4/index.js
@@ -13,20 +13,34 @@ dataByLine.forEach( function( value ) {
     }
 });
 
-let total = 0;
+let total1 = 0, total2 = 0;
 
 for (let i = 0; i < set1.length; i++) {
     if ( isContainedBy(set1[i], set2[i]) ) {
-        total += 1;
+        total1 += 1;
+    }
+    if ( overlaps(set1[i], set2[i])) {
+        total2 += 1;
     }
 }
 
-console.log(total);
+console.log("Part 1: contained by total", total1);
+console.log("Part 2: any overlap total", total2);
 
 function isContainedBy(array1, array2){
     if( array1[0] <= array2[0] && array1[1] >= array2[1] ||   // If array2 is completely contained in array1
         array2[0] <= array1[0] && array2[1] >= array1[1] )    // or array1 is completely contained in array2
         return true;
+}
+
+function overlaps(array1, array2) {
+    if (array1[1] < array2[0] ||
+        array1[0] > array2[1] ) {
+            return false;
+    }
+    else {
+        return true;
+    }
 }
 
 function getTwoHyphenatedNumbers(string){
